f46463c6104504dee4c3b266fff70746f5d829b3
g4bh <gabriella266@gmail.com>
2023-12-17 21:39:34 -0300

    upload do projeto

---
+++ WebApi-DatabaseFirst/Controllers/CustomerController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApi_DatabaseFirst.Models;
+
+namespace WebApi_DatabaseFirst.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : Controller
+    {
+        private readonly AdventureWorksLt2019Context _context;
+
+        public CustomerController(AdventureWorksLt2019Context context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Customer>>> GetCustomers()
+        {
+            List<Customer> customers = await _context.Customers.ToListAsync();
+            return Ok(customers);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Customer>> GetCustomer(int id)
+        {
+            Customer customer = await _context.Customers.FindAsync(id);
+            return Ok(customer);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteCustomer(int id)
+        {
+            Customer customer = await _context.Customers.FindAsync(id);
+
+            if (customer == null){
+                return BadRequest("Não encontrado");
+            }
+
+            _context.Customers.Remove(customer);
+            await _context.SaveChangesAsync();
+            return Ok("Deletado");
+        }
+
+    }
+}
---
+++ WebApi-DatabaseFirst/Program.cs
@@ -0,0 +1,30 @@
+using WebApi_DatabaseFirst.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Referencia do banco de dados para a aplicação
+builder.Services.AddDbContext<AdventureWorksLt2019Context>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
